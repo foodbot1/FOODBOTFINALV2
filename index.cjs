c803e21af10c2e0ffb2d9daf7f6118849d1ddb36
VITOR ELY DE OLIVEIRA NERY <vitor.representante1@gmail.com>
2025-05-12 12:12:36 -0300

    fix: ajustado PORT para compatibilidade Railway

--- index.cjs
+++ index.cjs
@@ -2,7 +2,7 @@ const express = require('express');
 const pagamentoRoutes = require('./routespagamento.cjs');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use('/', pagamentoRoutes);
 
